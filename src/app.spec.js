5bd1973fa8a8740fc17bad21d3c0a87dd6882d2d
Nir <niritzhak10.devel@gmail.com>
2023-06-27 19:57:31 +0300

    OK

---
+++ src/app.spec.js
@@ -0,0 +1,13 @@
+import chai, { expect } from "chai";
+// import { clone, cloneDeep, groupBy, rest } from "lodash-es";
+import { clone, cloneDeep, defaults } from "lodash-es";
+
+// import * as Restaurant from "../restaurant.js";
+
+chai.should();
+chai.expect();
+
+describe("Bankist", () => {
+  it("Test 1!", () => {});
+  expect(2 === 2).to.equal(true);
+});
